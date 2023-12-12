b77d5cf27d0ec61f8e5fc894b99932c4fe96b4e7
DawidGradowski <d.gradowski23@gmail.com>
2023-12-12 17:11:11 +0100

    Simple fix :+1:

--- Assets/Scripts/PauseMenu/PauseMenu.cs
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -50,7 +50,7 @@ public class PauseMenu : MonoBehaviour
     public void GoToMainMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("StartMenu");
     }
 
     public void QuitGame()

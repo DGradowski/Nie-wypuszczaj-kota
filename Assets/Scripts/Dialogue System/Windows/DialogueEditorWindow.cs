bcde7c7211631edd0c0b3801da39db68a16e8cf5
Dawid Gradowski <d.gradowski23@gmail.com>
2024-02-26 00:29:41 +0100

    Add Dialogue Editor Window

---
+++ Assets/Scripts/Dialogue System/Windows/DialogueEditorWindow.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace DSWindows
+{
+	public class DialogueEditorWindow : EditorWindow
+	{
+		[MenuItem("Window/DialogueSystem/DialogueGraph")]
+		public static void Open()
+		{
+			GetWindow<DialogueEditorWindow>("Dialogue Graph");
+		}
+
+		private void OnEnable()
+		{
+			AddGraphView();
+		}
+
+		private void AddGraphView()
+		{
+			DialogueGraphView graphView = new DialogueGraphView();
+
+			rootVisualElement.Add(graphView);
+		}
+	}
+}
---
+++ Assets/Scripts/Dialogue System/Windows/DialogueGraphView.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace DSWindows
+{
+	public class DialogueGraphView : GraphView
+	{
+	}
+}
